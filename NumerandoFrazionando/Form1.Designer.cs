d8917517c7b45d067f183d480c870d1090d379ca
Nico22F <nicofabbricatore22@gmail.com>
2024-01-23 11:53:22 +0100

    Inserito il bottone che permette di inserire / modificare la frazione

--- NumerandoFrazionando/Form1.Designer.cs
+++ NumerandoFrazionando/Form1.Designer.cs
@@ -28,21 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.inserisci_frazione = new System.Windows.Forms.Button();
+            this.numeratore_label = new System.Windows.Forms.Label();
+            this.denominatore_label = new System.Windows.Forms.Label();
+            this.riga = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // inserisci_frazione
+            // 
+            this.inserisci_frazione.Location = new System.Drawing.Point(344, 421);
+            this.inserisci_frazione.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.inserisci_frazione.Name = "inserisci_frazione";
+            this.inserisci_frazione.Size = new System.Drawing.Size(312, 81);
+            this.inserisci_frazione.TabIndex = 0;
+            this.inserisci_frazione.Text = "INSERISCI / MODIFICA FRAZIONE";
+            this.inserisci_frazione.UseVisualStyleBackColor = true;
+            this.inserisci_frazione.Click += new System.EventHandler(this.inserisci_frazione_Click);
+            // 
+            // numeratore_label
+            // 
+            this.numeratore_label.AutoSize = true;
+            this.numeratore_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numeratore_label.Location = new System.Drawing.Point(451, 173);
+            this.numeratore_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.numeratore_label.Name = "numeratore_label";
+            this.numeratore_label.Size = new System.Drawing.Size(99, 33);
+            this.numeratore_label.TabIndex = 1;
+            this.numeratore_label.Text = "label1";
+            // 
+            // denominatore_label
+            // 
+            this.denominatore_label.AutoSize = true;
+            this.denominatore_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.denominatore_label.Location = new System.Drawing.Point(451, 299);
+            this.denominatore_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.denominatore_label.Name = "denominatore_label";
+            this.denominatore_label.Size = new System.Drawing.Size(99, 33);
+            this.denominatore_label.TabIndex = 2;
+            this.denominatore_label.Text = "label2";
+            // 
+            // riga
+            // 
+            this.riga.AutoSize = true;
+            this.riga.Location = new System.Drawing.Point(408, 241);
+            this.riga.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.riga.Name = "riga";
+            this.riga.Size = new System.Drawing.Size(142, 16);
+            this.riga.TabIndex = 3;
+            this.riga.Text = "---------------------------";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(962, 515);
+            this.Controls.Add(this.riga);
+            this.Controls.Add(this.denominatore_label);
+            this.Controls.Add(this.numeratore_label);
+            this.Controls.Add(this.inserisci_frazione);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Frazionando22F";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button inserisci_frazione;
+        private System.Windows.Forms.Label numeratore_label;
+        private System.Windows.Forms.Label denominatore_label;
+        private System.Windows.Forms.Label riga;
     }
 }
 
--- NumerandoFrazionando/Form1.cs
+++ NumerandoFrazionando/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;
 
 namespace NumerandoFrazionando
 {
@@ -19,7 +20,86 @@ namespace NumerandoFrazionando
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            numeratore_label.Visible = false;
+            denominatore_label.Visible = false;
+            riga.Visible = false;
+        }
 
+        public class Frazione
+        {
+            private int numeratore;
+            private int denominatore;
+
+
+            public int Numeratore
+            { get { return numeratore; } set { numeratore = value; } }
+
+            public int Denominatore
+            { get { return denominatore; } set { denominatore = value; } }
+
+
+        }
+
+        // bottone che inserisce la frazione
+        private void inserisci_frazione_Click(object sender, EventArgs e)
+        {
+            // input modfica prodotto nome
+
+            string titolo_input = "Creazione Frazione", frase = "Inserisci il numeratore";
+            object input_numeratore = Interaction.InputBox(frase, titolo_input);
+
+            if ((string)input_numeratore == "") // user esce o lascia il campo vuoto
+            {
+                MessageBox.Show("Errore nell'aggiunta del numeratore", "ERRORE", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                int prova_numero = 0;
+
+                if (!int.TryParse((string)input_numeratore, out prova_numero)) // controllo che l'input sia di tipo intero
+                {
+                    MessageBox.Show("Errore nell'aggiunta del numeratore", "ERRORE", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else // input corretto
+                {
+                    int numeratore = prova_numero;
+
+                    // denominatore
+
+                    titolo_input = "Creazione Frazione";
+                    frase = "Inserisci il denominatore";
+                    object input_denominatore = Interaction.InputBox(frase, titolo_input);
+
+                    if ((string)input_denominatore == "") // user esce o lascia il campo vuoto
+                    {
+                        MessageBox.Show("Errore nell'aggiunta del denominatore", "ERRORE", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                    else
+                    {
+                        prova_numero = 0;
+
+                        if (!int.TryParse((string)input_denominatore, out prova_numero)) // controllo che l'input sia di tipo intero
+                        {
+                            MessageBox.Show("Errore nell'aggiunta del denominatore", "ERRORE", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
+                        else // input corretto
+                        {
+                            int denominatore = prova_numero;
+
+                            // mostro la frazione
+
+                            numeratore_label.Text = numeratore.ToString();
+                            numeratore_label.Visible = true;
+
+                            riga.Visible = true;
+
+                            denominatore_label.Text = denominatore.ToString();
+                            denominatore_label.Visible = true;
+                        }
+
+                    }
+                }
+            }
         }
     }
 }
